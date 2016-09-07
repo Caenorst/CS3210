05eb10745d32883e6b7ee0b980ef5d0cbc19f163
Caenorst <Caenorst@hotmail.com>
2016-09-07 14:34:55 +0800

    changed threads values to respect the exercise

--- exercise2.sh
+++ exercise2.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 gcc -fopenmp mm-omp.c -o mm1
 echo "calculation with matrice of size $1" > result2.txt
-for i in `seq 1 20`
+declare -a threads=(1 2 3 4 8 16 32)
+for i in "${threads[@]}"
 do
 	echo "$i threads:" >> result2.txt
 	perf stat -- ./mm1 $1 ${i} 2>> result2.txt

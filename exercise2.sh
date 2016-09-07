cf1e3c0f941727796303362b9b08d79323216c22
Caenorst <Caenorst@hotmail.com>
2016-09-07 14:12:53 +0800

    more complets results on exercise 2

--- exercise2.sh
+++ exercise2.sh
@@ -4,5 +4,5 @@ echo "calculation with matrice of size $1" > result2.txt
 for i in `seq 1 20`
 do
 	echo "$i threads:" >> result2.txt
-	perf stat -e cycles -e instructions -- ./mm1 $1 ${i} 2>> result2.txt
+	perf stat -- ./mm1 $1 ${i} 2>> result2.txt
 done
